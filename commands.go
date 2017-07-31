71a745c79efb88532a299b4d788f005709947a38
David R. Jenni <david.r.jenni@gmail.com>
2017-07-31 17:19:39 +0200

    Consolidate all command functions in one file.

--- commands.go
+++ commands.go
@@ -1,13 +1,17 @@
-// Copyright (c) 2016 David R. Jenni. All rights reserved.
+// Copyright (c) 2017 David R. Jenni. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
 )
 
 // callees shows possible targets of the selected function call
@@ -28,12 +32,6 @@ func callstack(s selection, args []string) {
 	fmt.Println(runWithStdin(archive(s), "guru", "-scope", scope(args), "-modified", "callstack", s.pos()))
 }
 
-// describe describes the selected syntax: definition, methods, etc.
-// using golang.org/x/tools/cmd/guru.
-func describe(s selection, args []string) {
-	fmt.Println(runWithStdin(archive(s), "guru", "-modified", "describe", s.pos()))
-}
-
 type GuruDef struct {
 	Objpos, Descr string
 }
@@ -52,10 +50,53 @@ func definition(s selection, args []string) {
 	}
 }
 
+// describe describes the selected syntax: definition, methods, etc.
+// using golang.org/x/tools/cmd/guru.
+func describe(s selection, args []string) {
+	fmt.Println(runWithStdin(archive(s), "guru", "-modified", "describe", s.pos()))
+}
+
+// godoc shows documentation for items in Go source code
+// using github.com/zmb3/gogetdoc.
+func godoc(s selection, args []string) {
+	fmt.Println(runWithStdin(archive(s), "gogetdoc", "-modified", "-pos", s.pos()))
+}
+
+// extract extracts statements to a new function/method
+// using github.com/godoctor/godoctor.
+func extract(s selection, args []string) {
+	if len(args) < 1 {
+		log.Fatal("Usage: A ex <name>")
+	}
+	pos := fmt.Sprintf("%d,%d", s.start, s.end-s.start)
+	code := run("godoctor", "-scope", ".", "-complete", "-file", s.filename, "-pos", pos, "extract", args[0])
+	if i := strings.Index(code, "\n"); i != -1 {
+		code = code[i+1:]
+	}
+	writeBody(s.win, code)
+	reloadShowAddr(s.win, s.start)
+}
+
 // freevars shows free variables of the selection
 // using golang.org/x/tools/cmd/guru.
 func freevars(s selection, args []string) {
 	fmt.Println(runWithStdin(archive(s), "guru", "-modified", "freevars", s.sel()))
+}
+
+// impl generates method stubs for implementing an interface
+// using github.com/josharian/impl.
+func impl(s selection, args []string) {
+	if len(args) < 2 {
+		log.Fatal("Usage: A impl <recv> <iface>")
+	}
+	l := len(args) - 1
+	code := run("impl", strings.Join(args[0:l], " "), args[l:][0])
+	end := ""
+	if s.start < len(s.body)-1 {
+		end = string(s.body[s.start+1:])
+	}
+	writeBody(s.win, string(s.body[:s.start])+"\n"+code+end)
+	reloadShowAddr(s.win, s.start)
 }
 
 // implements shows the 'implements' relation for the selected type or method
@@ -80,6 +121,36 @@ func pointsto(s selection, args []string) {
 // using golang.org/x/tools/cmd/guru.
 func referrers(s selection, args []string) {
 	fmt.Println(runWithStdin(archive(s), "guru", "-modified", "referrers", s.pos()))
+}
+
+// rename renames the selected identifier
+// using golang.org/x/tools/cmd/gorename.
+func rename(s selection, args []string) {
+	if len(args) < 1 {
+		log.Fatal("Usage: A rn <name>")
+	}
+	run("gorename", "-offset", s.pos(), "-to", args[0])
+	reloadShowAddr(s.win, s.start)
+}
+
+// share uploads the selected code to play.golang.org
+// and prints the generated URL.
+func share(s selection, args []string) {
+	body := bytes.NewReader(s.body[s.start:s.end])
+	req, err := http.NewRequest("POST", "https://play.golang.org/share", body)
+	if err != nil {
+		log.Fatalf("cannot send snippet: %v", err)
+	}
+	rsp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("cannot send snippet: %v", err)
+	}
+	defer rsp.Body.Close()
+	id, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		log.Fatalf("cannot send snippet: %v", err)
+	}
+	fmt.Printf("https://play.golang.org/p/%s\n", id)
 }
 
 // what shows basic information about the selected syntax node
